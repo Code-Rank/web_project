3f48914189b746485401c4517e5f8777a3069b6a
umair <ua220743@gmail.com>
2023-01-08 21:32:56 -0700

    create_appointment page

--- routes/userRoute/userRoutes.js
+++ routes/userRoute/userRoutes.js
@@ -10,6 +10,14 @@ router.get("/doctors",(req,res)=>{
     res.render('userView/doctors.ejs');
     //res.send("hello");
 });
+router.get("/create_appointment",(req,res)=>{
+    res.render('userView/create_appointment.ejs');
+    //res.send("hello");
+});
+router.get("/payment",(req,res)=>{
+    res.render('userView/payment.ejs');
+    //res.send("hello");
+});
 
 export default router;
 
