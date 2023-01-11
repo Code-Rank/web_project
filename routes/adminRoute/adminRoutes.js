7296b3342de65d3f45206641baff014e723755a8
umair <ua220743@gmail.com>
2023-01-11 08:18:11 -0700

    same

--- routes/adminRoute/adminRoutes.js
+++ routes/adminRoute/adminRoutes.js
@@ -4,5 +4,14 @@ const router=express.Router();
 router.get("/dashboard",(req,res)=>{
     res.render("adminView/Dashbord.ejs");
 });
+router.get("/add_doctor",(req,res)=>{
+    res.render("adminView/adddoctor.ejs");
+});
+router.get("/totol_appointment",(req,res)=>{
+    res.render("adminView/totalAppointment.ejs");
+});
+router.get("/user",(req,res)=>{
+    res.render("adminView/users.ejs");
+});
 
 export default router;
